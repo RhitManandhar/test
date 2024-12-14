0cf49f35c49cef5432a12900bf3b2f36cbad5d49
Rhit Manandhar <rhit_m@192-168-1-6.tpgi.com.au>
2024-12-14 20:33:55 +1100

    feat: stream audio from youtube

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,68 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import playLogo from '/play.svg'
+import stopLogo from '/stop.svg'
+import pauseLogo from '/pause.svg'
 import './App.css'
+import YouTube from "react-youtube";
+
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [player, setPlayer] = useState(null);
+    const [isPaused, setIsPaused] = useState(false);
+  const [song, setSong] = useState('nIhs1T7OcZg');
+    const opts = {
+        height: "0", // Hide the video
+        width: "0",
+        playerVars: {
+            controls: 0, // Hide controls
+            modestbranding: 1, // Remove YouTube logo
+            rel: 0, // Disable related videos
+        },
+    };
+    const onReady = (event:any) => {
+        setPlayer(event.target);
+    };
+
+    const play = () => {
+        if (player) player.playVideo();
+        setIsPaused(true);
+    };
+
+    const pause = () => {
+        console.log('pause track');
+        if (player) player.pauseVideo();
+        setIsPaused(false);
+    };
+    const stop = ()=>{
+        console.log('stop track');
+        if (player) player.stopVideo();
+        setIsPaused(false);
+    }
+    const getVideoId = (url:string) => {
+        console.log('getVideoId', url);
+        const urlObj = new URL(url);
+        setSong (urlObj.searchParams.get("v")||'nIhs1T7OcZg')
+        console.log('getSongId', song);
+
+    };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <>
+          <div className="form__group field" >
+              <textarea className="form__field" placeholder="Name" name="name" id='name' onChange={(e) => getVideoId(e.target.value)}/>
+              <label htmlFor="name" className="form__label">Link</label>
+          </div>
+          <div className="mainBox">
+              <YouTube videoId={song} opts={opts} onReady={onReady}/>
+              <button onClick={isPaused?pause:play}>
+                  <img src={isPaused?pauseLogo:playLogo} className="logo" alt="Play Pause Logo"/>
+              </button>
+              <button onClick={stop}>
+                  <img src={stopLogo} className="logo" alt="Stop Logo"/>
+              </button>
+
+          </div>
+      </>
   )
 }
 
