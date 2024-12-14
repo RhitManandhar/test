a911cb4a66a07c502c6f460b469d24ea6327264d
Rhit Manandhar <rhit_m@192-168-1-6.tpgi.com.au>
2024-12-14 21:17:35 +1100

    feat: stop music at random time

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,15 @@ import playLogo from '/play.svg'
 import stopLogo from '/stop.svg'
 import pauseLogo from '/pause.svg'
 import './App.css'
-import YouTube, {YouTubePlayer} from "react-youtube";
+import YouTube, {YouTubeEvent, YouTubePlayer} from "react-youtube";
+import {getRndNumber} from "./utils/helper.ts";
 
 
 function App() {
     const [player, setPlayer] = useState<YouTubePlayer|null>(null);
     const [isPaused, setIsPaused] = useState(false);
   const [song, setSong] = useState('nIhs1T7OcZg');
+  const [limit, setLimit] = useState<number>(60);
     const opts = {
         height: "0", // Hide the video
         width: "0",
@@ -19,13 +21,19 @@ function App() {
             rel: 0, // Disable related videos
         },
     };
-    const onReady = (event:any) => {
+    const onReady = (event: YouTubeEvent) => {
         setPlayer(event.target);
     };
 
     const play = () => {
         if (player) player.playVideo();
         setIsPaused(true);
+        const randomNumber = getRndNumber(10,limit);
+        console.log('randomNumber', randomNumber);
+        setTimeout(()=>{
+            console.log('Timeout !!')
+            pause()
+        },randomNumber*1000)
     };
 
     const pause = () => {
@@ -48,19 +56,25 @@ function App() {
 
   return (
       <>
-          <div className="form__group field" >
-              <textarea className="form__field" placeholder="Name" name="name" id='name' onChange={(e) => getVideoId(e.target.value)}/>
+          <h4>Minimum Timer is set to 10 seconds</h4>
+          <div className="form__group field">
+              <input className="form__field" placeholder="50" name="timer" id='timer' type='number'
+                        onChange={(e) => setLimit(parseInt(e.target.value))}/>
+              <label htmlFor="timerLimit" className="form__label">Timer Limit</label>
+          </div>
+          <div className="form__group field">
+              <textarea className="form__field" placeholder="Link" name="link" id='link'
+                        onChange={(e) => getVideoId(e.target.value)}/>
               <label htmlFor="name" className="form__label">Link</label>
           </div>
           <div className="mainBox">
               <YouTube videoId={song} opts={opts} onReady={onReady}/>
-              <button onClick={isPaused?pause:play}>
-                  <img src={isPaused?pauseLogo:playLogo} className="logo" alt="Play Pause Logo"/>
+              <button onClick={isPaused ? pause : play}>
+                  <img src={isPaused ? pauseLogo : playLogo} className="logo" alt="Play Pause Logo"/>
               </button>
               <button onClick={stop}>
                   <img src={stopLogo} className="logo" alt="Stop Logo"/>
               </button>
-
           </div>
       </>
   )
